fc67317b693f210d816c9f4284f6d81d10beba93
mahoska <annamakhovskayawm@gmail.com>
2020-11-04 19:00:57 +0200

    added tabs

---
+++ force-app/main/default/aura/carDetails/carDetailsController.js
@@ -0,0 +1,15 @@
+/**
+ * @description       : 
+ * @author            : Anna Makhovskaya
+ * @group             : 
+ * @last modified on  : 11-04-2020
+ * @last modified by  : Anna Makhovskaya
+ * Modifications Log 
+ * Ver   Date         Author             Modification
+ * 1.0   11-04-2020   Anna Makhovskaya   Initial Version
+**/
+({
+    handleTabActive: function (component, event, helper) {
+        helper.loadTabs(component, event);
+    }
+})
