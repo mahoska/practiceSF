b92f741964ddfedd06427760ebf3d780653f1512
mahoska <annamakhovskayawm@gmail.com>
2020-11-16 18:30:56 +0200

    --

--- force-app/main/default/aura/carDetail/carDetailController.js
+++ force-app/main/default/aura/carDetail/carDetailController.js
@@ -2,14 +2,24 @@
  * @description       : 
  * @author            : Anna Makhovskaya
  * @group             : 
- * @last modified on  : 11-13-2020
+ * @last modified on  : 11-16-2020
  * @last modified by  : Anna Makhovskaya
  * Modifications Log 
  * Ver   Date         Author             Modification
  * 1.0   11-13-2020   Anna Makhovskaya   Initial Version
 **/
 ({
-    fullDetailsHandler: function (component, event, helper) {
+    doInit: function (component, event, helper) {
+        helper.loadCarData(component);
+    },
 
+    handleCarIdChange: function (component, event, helper) {
+        helper.loadCarData(component);
+    },
+
+
+    fullDetailsHandler: function (component, event, helper) {
+        console.log('in...');
+        helper.navigateToCarRecord(component);
     }
 })
--- force-app/main/default/aura/carDetail/carDetailHelper.js
+++ force-app/main/default/aura/carDetail/carDetailHelper.js
@@ -1,5 +1,49 @@
+/**
+ * @description       : 
+ * @author            : Anna Makhovskaya
+ * @group             : 
+ * @last modified on  : 11-16-2020
+ * @last modified by  : Anna Makhovskaya
+ * Modifications Log 
+ * Ver   Date         Author             Modification
+ * 1.0   11-16-2020   Anna Makhovskaya   Initial Version
+**/
 ({
-    helperMethod : function() {
+    loadCarData: function (cmp) {
+        var action = cmp.get("c.getCarById");
+        action.setParams({
+            "carId": cmp.get("v.carId")
+        });
+        action.setCallback(this, function (response) {
+            var state = response.getState();
 
+            if (state === "SUCCESS") {
+                cmp.set("v.isCarFound", true);
+                cmp.set("v.car", response.getReturnValue());
+            }
+            else {
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    "title": "Error!",
+                    "message": " Something has gone wrong."
+                });
+                toastEvent.fire();
+
+                cmp.set("v.isCarFound", false);
+            }
+
+        });
+        $A.enqueueAction(action);
+    },
+
+    navigateToCarRecord: function (cmp) {
+        console.log('in-');
+        var navEvent = $A.get("e.force:navigateToSObject");
+        navEvent.setParams({
+            recordId: cmp.get("v.car").Id,
+            slideDevName: "detail"
+        });
+        navEvent.fire();
     }
+
 })
